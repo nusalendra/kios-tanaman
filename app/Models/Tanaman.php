7e912c7cc0eb93c06f850512da7db6e03ff015b3
Nusalendra <nusalendraa@gmail.com>
2024-10-22 22:16:32 +0700

    refactor:tambah kolom tanaman

--- app/Models/Tanaman.php
+++ app/Models/Tanaman.php
@@ -10,7 +10,7 @@ class Tanaman extends Model
     use HasFactory;
     protected $table = 'tanamans';
     protected $primarykey = 'id';
-    protected $fillable = ['nama'];
+    protected $fillable = ['nama', 'harga', 'foto'];
     protected $guraded = [];
 
     public function kriteria()
